504553b739f11436dc37e789fde1117f3c25f077
haider <haider.shah@hotmail.com>
2019-09-02 03:06:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(ar)
+  i=0
+  while i< ar.length do
+    ar[i]="#{i+1}. #{ar[i]}"
+    i+=1
+  end
+    ar.map do |b|
+      puts b
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(ar)
+  ar.map do |b|
+    "#{b.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(ar)
+  ar.map do |b|
+    if b.length>4 
+return true 
 end
+end
+ar.map do |b|
+    if b.length<=4
+      return false
+    end
+  end
+    
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
